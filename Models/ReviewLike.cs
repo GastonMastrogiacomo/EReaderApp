67ac53a4bd379eaad77aad68702254439771f7b4
Gastón Mastrogiacomo <64843482+GastonMastrogiacomo@users.noreply.github.com>
2025-05-04 22:42:33 -0300

    .

---
+++ Models/ReviewLike.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EReaderApp.Models
+{
+    public class ReviewLike
+    {
+        [Key]
+        public int FKIdUser { get; set; }
+
+        [Key]
+        public int FKIdReview { get; set; }
+
+        [ForeignKey("FKIdUser")]
+        public User User { get; set; }
+
+        [ForeignKey("FKIdReview")]
+        public Review Review { get; set; }
+    }
+}
